2f5a96895231b73668ecd51c1716d6d9444598e1
jesusperez5 <jesuspepr554@gmail.com>
2024-12-10 17:03:36 +0100

    Creando fragment

---
+++ app/src/main/java/com/odeene/fragment/MainActivity.java
@@ -0,0 +1,48 @@
+package com.odeene.fragment;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        EditText input = findViewById(R.id.editTextText);
+        Button boton = findViewById(R.id.button);
+        BlankFragment displayFragment = new BlankFragment();;
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.displayFragment, displayFragment)
+                    .commit();
+        }
+
+        boton.setOnClickListener(v -> {
+            String inputText = input.getText().toString();
+            // Llamar al método del Fragment para actualizar el texto
+            if (displayFragment != null) { Log.i("error" , "adsdsdawd");
+                displayFragment.updateText(inputText);
+            } else {
+                Log.i("error" , "adsd");
+            }
+        });
+    }
+}
